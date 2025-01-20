ef37204d67b0d7ab25b349f723c1e00ec3c1e53d
subinny <116608047+subinny2@users.noreply.github.com>
2025-01-20 22:35:59 +0900

    25.1.20 코딩 기초 트레이닝

---
+++ src/Codekata10.java
@@ -0,0 +1,15 @@
+public class Codekata10 {
+    static class Solution10 {
+        public int solution10(int a, int d, boolean[] included) {
+            int answer = 0;
+            return answer;
+        }
+
+        public static void main(String[] args){
+            Codekata10.Solution10 sol10 = new Codekata10.Solution10();
+            boolean[] included = {true, false, false, true, true};
+            int result = sol10.solution10(3, 4, included);
+            System.out.println(result);
+        }
+    }
+}
