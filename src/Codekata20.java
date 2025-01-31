882f9bbe7c7d3f58d982eddf0a52977f35069dc0
subinny <116608047+subinny2@users.noreply.github.com>
2025-01-31 16:19:57 +0900

    25.1.31 코딩 기초 트레이닝

---
+++ src/Codekata20.java
@@ -0,0 +1,19 @@
+public class Codekata20 {
+    static class Solution20 {
+        public int[] solution20(int l, int r) {
+            int[] answer = {};
+            for(int i=l; i<=r; i++){
+                if(i)
+            }
+            return answer;
+        }
+
+        public static void main(String[] args){
+            Codekata20.Solution20 sol20 = new Codekata20.Solution20();
+            int[] result = sol20.solution20(5,555);
+//            int[] result = sol20.solution20(10,20);
+            System.out.println(result);
+
+        }
+    }
+}
